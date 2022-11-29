0f379f5a172b492a68f772cc3f12e7ea95a283e8
AhmadHere1 <aghazi1011@gmail.com>
2022-11-30 00:42:15 +0500

    done 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
-
 function App() {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    getUser();
+  }, [])
+
+  //axios function
+  async function getUser() {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        console.log(response)
+        const data = response.data.slip.advice;
+        setQuote(data);
+
+      })
+      .catch((error) => {
+        console.log(error)
+      }
+      )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">
+          {quote}
+        </h1>
+        <div >
+          <button
+            onClick={getUser}
+            className="button">
+            <span>Get New Quote</span>
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
